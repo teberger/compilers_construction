b5e59cfa232003bb3ea0540043290edb612d9173
teberger <tberge01@cs.unm.edu>
2014-08-20 14:47:53 -0600

    /home/tberger/git/compilers_construction/warmup1/emulator.hs autocommit from emacs: Wed Aug 20 14:47:53 2014

--- warmup1/emulator.hs
+++ warmup1/emulator.hs
@@ -2,6 +2,23 @@ import Control.IO
 import Data.Bits
 import Data.Int
 
+data OptCode = Opt1 "" |
+               Array_Index |
+               Array_Update | 
+               Addition | 
+               Multiplication | 
+               Division |
+               Nand |
+               Halt |
+               Allocation |
+               Deallocation | 
+               Output |
+               Input | 
+               Load |
+               Load_Immediate
+
 main :: IO ()
 main = return ()
 
+opCode :: Int32 -> OptCode
+opCode = undefined
