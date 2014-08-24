3b59db7f007aae85ed814f877fe2d8b52a9e555e
teberger <tberge01@cs.unm.edu>
2014-08-24 12:36:02 -0600

    /home/tberger/git/compilers_construction/warmup1/emulator.hs autocommit from emacs: Sun Aug 24 12:36:01 2014

--- warmup1/emulator.hs
+++ warmup1/emulator.hs
@@ -37,7 +37,7 @@ condMove inst = do
   let idxA = inst .&. 0xE0
       idxB = inst .&. 0x1B
       idxC = inst .&. 0x7
-      newRegisters = [ r | r <- regs, (regs !! idxC) != 0]
+      newRegisters = [ r | r <- regs, (regs !! idxC) /= 0]
   
   
   return (regs, mem)
