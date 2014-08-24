5cab880af8e78646f9732ef04d0bf355978ef793
teberger <tberge01@cs.unm.edu>
2014-08-24 12:39:06 -0600

    /home/tberger/git/compilers_construction/warmup1/emulator.hs autocommit from emacs: Sun Aug 24 12:39:06 2014

--- warmup1/emulator.hs
+++ warmup1/emulator.hs
@@ -34,11 +34,9 @@ opCode instruction =  toEnum . fromIntegral $ (instruction .&. (0x0007))
 condMove :: Int32 -> State MachineState MachineState
 condMove inst = do
   (regs, mem) <- get
-  let idxA = fromIntegral $ inst .&. 0xE0
-      idxB = fromIntegral $ inst .&. 0x1B
-      idxC = fromIntegral $ inst .&. 0x7
-      newRegisters = [ r | r <- regs, (regs !! idxC) == 0]
-  
+  let regA = regs !! fromIntegral $ inst .&. 0xE0
+      regB = regs !! fromIntegral $ inst .&. 0x1B
+      regC = regs !! fromIntegral $ inst .&. 0x7
   
   return (regs, mem)
 --}
