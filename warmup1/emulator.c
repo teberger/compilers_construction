8030b99af63e49ccc9586ae5a7d93c7a075989ad
teberger <tberge01@cs.unm.edu>
2014-08-25 14:46:21 -0600

    /home/tberger/git/compilers_construction/warmup1/emulator.c autocommit from emacs: Mon Aug 25 14:46:21 2014

--- warmup1/emulator.c
+++ warmup1/emulator.c
@@ -43,7 +43,7 @@ void conditional_move();
 void set_instruction();
 
 int main() {
-  uint32_t i = 0b10000000000000000000000111110101;
+  uint32_t i = 0x800001F5;
   set_instruction(i);
   conditional_move();
 }
