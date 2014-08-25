b75ec1f9746a7bdde944da9c8b29b0ce54f0ac59
teberger <tberge01@cs.unm.edu>
2014-08-25 14:13:45 -0600

    /home/tberger/git/compilers_construction/warmup1/emulator.c autocommit from emacs: Mon Aug 25 14:13:45 2014

---
+++ warmup1/emulator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct
+{
+  unsigned int regA   : 3;
+  unsigned int regB   : 3;
+  unsigned int regC   : 3;
+  unsigned int value  : 19;
+  unsigned int opcode : 4;
+} Instruction;
+
+enum 
+{
+
+};
+
+int main() {
+
+}
